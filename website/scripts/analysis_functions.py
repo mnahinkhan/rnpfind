a2f9650ca076efbf2f36f5c32f93e631613686cd
mnahinkhan <mnk1@andrew.cmu.edu>
2020-12-18 03:03:43 +0300

    improved style of analysis_functions

--- website/scripts/analysis_functions.py
+++ website/scripts/analysis_functions.py
@@ -1,21 +1,30 @@
-# This file is responsible for managing all the analysis functions that RNPFind supports. If you want to develop and
-# incorporate your own analysis function into RNPFind, this is the file to edit!
+"""
+This file is responsible for managing all the analysis functions that RNPFind
+supports. If you want to develop and incorporate your own analysis function into
+RNPFind, this is the file to edit!
 
-# The way any analysis function works in RNPFind is as follows. RNPFind will call the analysis function like this:
-# analysis_method_function_name(bigStorage, RNAInfo)
+The way any analysis function works in RNPFind is as follows. RNPFind will call
+the analysis function like this:
 
-# where bigStorage is a dictionary that maps a data_source string to a Storage variable (which in turn maps rbp names to
-# a list of binding sites; for more details see load_data.py), and RNAInfo is a list that stores the RNA name and its
-# chromosomal location on the hg38 chromosome. If you preferred to have the RNA sequence, get_human_seq() from pwm_scan
-# should help you (see attract_data_load.py for an example of that).
+analysis_method_function_name(bigStorage, RNAInfo)
 
-# Upon getting the above input, an analysis function is free to do with the information as it pleases. The
-# information it outputs to the user could be in any form, such as text on the CLI, csv files saved on the local
-# directory, heat maps produced on a figure generated, or a URL to web-generated content. Once done with the work, the
-# function should return so that RNPFind can continue with its process.
+where bigStorage is a dictionary that maps a data_source string to a Storage
+variable (which in turn maps rbp names to a list of binding sites; for more
+details see load_data.py), and RNAInfo is a list that stores the RNA name and
+its chromosomal location on the hg38 chromosome. If you preferred to have the
+RNA sequence, get_human_seq() from pwm_scan should help you (see
+attract_data_load.py for an example of that).
 
-# If an analysis function is made to the above specification, the below lines should be edited to incorporate the
-# function on RNPFind!
+Upon getting the above input, an analysis function is free to do with the
+information as it pleases. The information it outputs to the user could be in
+any form, such as text on the CLI, csv files saved on the local directory, heat
+maps produced on a figure generated, or a URL to web-generated content. Once
+done with the work, the function should return so that RNPFind can continue with
+its process.
+
+If an analysis function is made to the above specification, the below lines
+should be edited to incorporate the function on RNPFind!
+# """
 
 # First, import the function from wherever it is defined:
 from .overAllCorrAnalysis import overall_correlation_analysis
@@ -26,9 +35,12 @@ from .ucsc_visualize import ucsc_visualize
 analysis_methods_supported_short = ['binding', 'per_binding', 'ucsc']
 
 # Give your method a long name:
-analysis_methods_supported_long = ["Binding correlation analysis", "Per-binding-site analysis",
+analysis_methods_supported_long = ["Binding correlation analysis",
+                                   "Per-binding-site analysis",
                                    "Visualize on UCSC Genome Browser"]
 
-# Map your short name to the variable imported above that corresponds to your function!
-analysis_method_functions = {'binding': overall_correlation_analysis, 'per_binding': per_binding_site_analysis,
+# Map your short name to the variable imported above that corresponds to your
+# function!
+analysis_method_functions = {'binding': overall_correlation_analysis,
+                            'per_binding': per_binding_site_analysis,
                              'ucsc': ucsc_visualize}
