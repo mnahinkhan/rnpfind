cf1c1a720c0b0796d45588457547c3b0ad219ad4
mnahinkhan <mnk1@andrew.cmu.edu>
2021-04-19 16:49:26 +0300

    conveinent cleanup script

---
+++ cleanup.py
@@ -0,0 +1,9 @@
+from website.models import Gene, AnalysisStatus
+
+bad_records = [
+    x for x in AnalysisStatus.objects.all()
+    if not Gene.objects.filter(name=x.request_id.exists())
+]
+
+for bad_record in bad_records:
+    bad_record.delete()
