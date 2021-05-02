1ca382243d12f12176df730fbd9e5adcacee7a7c
mnahinkhan <mnk1@andrew.cmu.edu>
2021-05-02 19:37:53 +0300

    added initial tests

---
+++ website/scripts/test_analysis_functions.py
@@ -0,0 +1,82 @@
+"""
+Tests the RNPFind analysis functions module for internal consistency.
+
+"""
+import unittest
+import inspect
+
+from .analysis_functions import (
+    analysis_methods_supported_short,
+    analysis_methods_supported_long,
+    analysis_method_functions
+)
+
+class TestAnalysisMethods(unittest.TestCase):
+    """
+    Tests to ensure that the analysis method related variables are properly set
+    up.
+
+    """
+
+    def test_equal_number_of_methods(self):
+        """
+        Checks that the number of analysis functions are equal across variables
+        describing them.
+
+        """
+        self.assertEqual(
+            len(analysis_methods_supported_short),
+            len(analysis_methods_supported_long)
+        )
+        self.assertEqual(
+            len(analysis_method_functions),
+            len(analysis_methods_supported_short)
+        )
+
+    def test_appropriate_name(self):
+        """
+        Checks that the analysis functions are given appropriate 'short' names.
+        """
+        for short_name, long_name in zip(
+            analysis_methods_supported_short,
+            analysis_methods_supported_long
+        ):
+            for token in short_name.split('_'):
+                self.assertIn(token.lower(), long_name.lower())
+
+
+    def test_method_dict_keys(self):
+        """
+        Checks whether analysis_method_functions describes the functions listed
+        in analysis_methods_supported_short.
+
+        """
+
+        # equality of size from a previous test along with injection here
+        # assures bijection.
+        for method in analysis_methods_supported_short:
+            self.assertIn(method, analysis_method_functions)
+
+
+    def test_appropriate_method_type(self):
+        """
+        Checks that each of the analysis methods have a consistent type
+        signature (only enforced for input; return type is different depending
+        on analysis type)
+
+        """
+
+        for method in analysis_method_functions.values():
+            self.assertEqual(
+                list(inspect.signature(method).parameters)[0],
+                'big_storage'
+            )
+
+            self.assertEqual(
+                list(inspect.signature(method).parameters)[1],
+                'rna_info'
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ website/scripts/test_attract_data_load.py
@@ -0,0 +1,40 @@
+"""
+Tests the Attract data loading functions for correctness
+
+"""
+import unittest
+
+from .attract_data_load import attract_data_load
+from .gene_coordinates import Chromosome
+class TestCorrectBindingSites(unittest.TestCase):
+    """
+    Tests to ensure that the ATTRACT data load function loads binding sites
+    that are consistent with that of the web interface.
+
+    """
+
+    def test_correct_sites(self):
+        """
+        Checks that the binding sites match with web interface.
+
+        """
+
+        test_rna_info = {}
+        test_rna_info['official_name'] = 'Malat1'
+        test_rna_info['chr_n'] = Chromosome(11)
+        test_rna_info['start_coord'] = 65497738
+        test_rna_info['end_coord'] = 65506516
+
+        attract_binding_sites = attract_data_load(test_rna_info)
+        self.assertEqual(
+            len(analysis_methods_supported_short),
+            len(analysis_methods_supported_long)
+        )
+        self.assertEqual(
+            len(analysis_method_functions),
+            len(analysis_methods_supported_short)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
