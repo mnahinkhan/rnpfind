15d6fe206bf1979b3ac6077f4b228dc0d3b761b3
mnahinkhan <mnk1@andrew.cmu.edu>
2021-07-10 01:11:40 +0300

    Fix bug: handle permission error on caching

--- cli/src/rnpfind/picklify.py
+++ cli/src/rnpfind/picklify.py
@@ -8,6 +8,7 @@ saved pickle files the next time around.
 
 """
 import pickle
+import sys
 from pathlib import Path
 
 from .config import PICKLE_PATH
@@ -39,9 +40,16 @@ def picklify(dict_generator, *args, **kwargs):
             dict_to_return = pickle.load(pickle_handle)
     except FileNotFoundError:
         dict_to_return = dict_generator(*args, **kwargs)
-        Path(pickle_path).parent.mkdir(parents=True, exist_ok=True)
-        with open(pickle_path, "wb") as pickle_handle:
-            pickle.dump(
-                dict_to_return, pickle_handle, protocol=pickle.HIGHEST_PROTOCOL
+        try:
+            Path(pickle_path).parent.mkdir(parents=True, exist_ok=True)
+            with open(pickle_path, "wb") as pickle_handle:
+                pickle.dump(
+                    dict_to_return,
+                    pickle_handle,
+                    protocol=pickle.HIGHEST_PROTOCOL,
+                )
+        except PermissionError:
+            print(
+                "Caching failed due to permission errors...", file=sys.stderr
             )
     return dict_to_return
